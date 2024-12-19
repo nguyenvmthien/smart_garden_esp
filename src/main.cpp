313b1f083506a8abe1f59fcffb1e253197af58f7
04nhatminh <04nhatminh@gmail.com>
2024-12-19 20:43:05 +0700

    NMinh: transfer data from ESP to Web using MQTT

--- src/main.cpp
+++ src/main.cpp
@@ -1,25 +1,83 @@
 #include <DHTesp.h>
 #include <WiFi.h>
-#include <HTTPClient.h>
+#include <PubSubClient.h>
+#include <WiFiClientSecure.h>
 
+// Kết nối WiFi
 const char *ssid = "Wokwi-GUEST";
 const char *password = "";
 
-const String BOT_TOKEN = "7171674667:AAGaMK5_sw0E4Dx7rI8YH3l5ZB1uHsCiXWE"; // Token của bot Telegram
-const String CHANNEL_ID = "1501781748";
+// Set server
+const char *mqttServer = "66e7cd73955d41119db073a43925b11b.s1.eu.hivemq.cloud";
+int port = 8883;
+const char *mqttUser = "smart-garden-esp32";
+const char *mqttPassword = "Username1";
+
+WiFiClientSecure wifiClient;
+PubSubClient mqttClient(wifiClient);
+
+const int wifiLedPin = 16;
+void wifiConnect()
+{
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED)
+  {
+    delay(500);
+    Serial.print(".");
+  }
+  Serial.println(" Connected!");
+  digitalWrite(wifiLedPin, HIGH);
+  delay(5000);
+  digitalWrite(wifiLedPin, LOW);
+}
+
+void mqttConnect()
+{
+  while (!mqttClient.connected())
+  {
+    Serial.println("Attempting MQTT connection...");
+    String clientId = "ESP32Client-" + String(random(0xffff), HEX);
+    if (mqttClient.connect(clientId.c_str(), mqttUser, mqttPassword))
+    {
+      Serial.println("MQTT connected");
+      mqttClient.subscribe("smart-garden/#");
+    }
+    else
+    {
+      Serial.print("MQTT connection failed, rc=");
+      Serial.println(mqttClient.state());
+      delay(5000);
+    }
+  }
+}
+
+// MQTT Receiver
+void callback(char *topic, byte *message, unsigned int length)
+{
+  Serial.println(topic);
+  String strMsg;
+  for (int i = 0; i < length; i++)
+  {
+    strMsg += (char)message[i];
+  }
+  Serial.println(strMsg);
+
+  //***Code here to process the received package***
+}
 
 // Cài đặt chân
 const int trigPin = 4;
 const int echoPin = 0;
 const int photoResistorPin = 32;
 DHTesp dhtSensor;
+
 const int dhtPin = 17;
+
 const int lightPin = 25;
 const int fanPin = 26;
 const int waterPin = 27;
 const int heaterPin = 14;
 const int pumpPin = 12;
-const int wifiLedPin = 16;
 
 // Ngưỡng cài đặt
 double minTemp = 25.0;
@@ -42,53 +100,8 @@ unsigned long lastWateringTime = 0;
 unsigned long lastHeaterTime = 0;
 unsigned long lastFanTime = 0;
 
-  void wifiConnect()
-  {
-    WiFi.begin(ssid, password);
-    while (WiFi.status() != WL_CONNECTED)
-    {
-      delay(500);
-      Serial.print(".");
-    }
-    digitalWrite(wifiLedPin, HIGH);
-    delay(2000);
-    digitalWrite(wifiLedPin, LOW);
-    Serial.println(" Connected!");
-  }
-
-void sendTelegramMessage(String message)
-{
-  if (WiFi.status() == WL_CONNECTED)
-  {
-    HTTPClient http;
-    String url = "https://api.telegram.org/bot" + BOT_TOKEN +
-                 "/sendMessage?chat_id=" + CHANNEL_ID +
-                 "&text=" + message;
-
-    http.begin(url);
-    int httpCode = http.GET();
-
-    if (httpCode > 0)
-    {
-      Serial.println("Telegram message sent successfully!");
-    }
-    else
-    {
-      Serial.println("Error sending message: " + http.errorToString(httpCode));
-    }
-    http.end();
-  }
-  else
-  {
-    Serial.println("WiFi not connected!");
-  }
-}
-
 void setup()
 {
-  Serial.begin(115200);
-  Serial.println("ESP32 Automation System Starting...");
-
   // Cài đặt chân
   pinMode(trigPin, OUTPUT);
   pinMode(echoPin, INPUT);
@@ -103,7 +116,16 @@ void setup()
   // Cài đặt cảm biến DHT
   dhtSensor.setup(dhtPin, DHTesp::DHT22);
 
+  Serial.begin(115200);
+  Serial.println("ESP32 Automation System Starting...");
+  Serial.println("Connecting to WiFi...");
   wifiConnect();
+  // Cấu hình MQTT Client
+  wifiClient.setInsecure();
+  mqttClient.setServer(mqttServer, port);
+
+  // Kết nối MQTT
+  mqttConnect();
 }
 
 double getDistance()
@@ -129,12 +151,15 @@ double getBrightness()
 
 void loop()
 {
+  // Nhận ngưỡng từ web
+  // ...
+
   unsigned long currentMillis = millis();
 
   // Đọc dữ liệu cảm biến
   TempAndHumidity data = dhtSensor.getTempAndHumidity();
   double brightness = getBrightness();
-  double waterLevel = tankHeight - getDistance();
+  double waterLevel = tankHeight - getDistance() > 0 ? tankHeight - getDistance() : 0;
 
   // Hiển thị thông số
   Serial.println("Temperature: " + String(data.temperature) + "°C");
@@ -145,7 +170,8 @@ void loop()
 
   // Kiểm tra và bật hệ thống nước
   if (data.temperature >= maxTemp || data.humidity <= minHumid)
-  {         
+  {
+    Serial.println(currentMillis - lastWateringTime);
     if (currentMillis - lastWateringTime >= defaultWateringDuration)
     {
       Serial.println("Watering: OFF");
@@ -240,6 +266,40 @@ void loop()
   {
     Serial.println("Light: OFF");
     digitalWrite(lightPin, LOW);
+  }
+
+  //***Check connection to MQTT Server***
+  if (!mqttClient.connected())
+  {
+    mqttConnect();
+  }
+  mqttClient.loop();
+
+  //***Publish data to MQTT Server***
+  if (!isnan(data.temperature))
+  {
+    char tempBuffer[50];
+    sprintf(tempBuffer, "%.1f", data.temperature);
+    mqttClient.publish("smart-garden/temperature", tempBuffer);
+  }
+  if (!isnan(data.humidity))
+  {
+    char tempBuffer[50];
+    sprintf(tempBuffer, "%.1f", data.humidity);
+    mqttClient.publish("smart-garden/humidity", tempBuffer);
+  }
+  if (!isnan(brightness))
+  {
+    char tempBuffer[50];
+    sprintf(tempBuffer, "%.1f", brightness);
+    mqttClient.publish("smart-garden/brightness", tempBuffer);
+    Serial.println("Brightness: " + String(brightness) + " lux");
+  }
+  if (!isnan(waterLevel))
+  {
+    char tempBuffer[50];
+    sprintf(tempBuffer, "%.1f", waterLevel);
+    mqttClient.publish("smart-garden/water-level", tempBuffer);
   }
   delay(1000);
 }
